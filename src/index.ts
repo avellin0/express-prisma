bb4ac16fb6a4de5536bdad96d9920bbfde2579ff
avellin0 <daviavelino2007@gmail.com>
2025-05-22 18:39:44 -0300

    feat: config initial version of project

--- src/index.ts
+++ src/index.ts
@@ -10,20 +10,18 @@ app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
-app.get("/todos", async (req, res) => {
-  const todos = await prisma.todo.findMany({
-    orderBy: { createdAt: "desc" },
-  });
+app.get("/artists", async (req, res) => {
+  const todos = await prisma.artist.findMany();
 
   res.json(todos);
 });
 
-app.post("/todos", async (req, res) => {
-  const todo = await prisma.todo.create({
+app.post("/artist", async (req, res) => {
+  const { name } = req.body;
+
+  const todo = await prisma.artist.create({
     data: {
-      completed: false,
-      createdAt: new Date(),
-      text: req.body.text ?? "Empty todo",
+      name,
     },
   });
 
@@ -32,30 +30,11 @@ app.post("/todos", async (req, res) => {
 
 app.get("/todos/:id", async (req, res) => {
   const id = req.params.id;
-  const todo = await prisma.todo.findUnique({
+  const todo = await prisma.artist.findUnique({
     where: { id },
   });
 
   return res.json(todo);
-});
-
-app.put("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.todo.update({
-    where: { id },
-    data: req.body,
-  });
-
-  return res.json(todo);
-});
-
-app.delete("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  await prisma.todo.delete({
-    where: { id },
-  });
-
-  return res.send({ status: "ok" });
 });
 
 app.get("/", async (req, res) => {
@@ -67,10 +46,10 @@ app.get("/", async (req, res) => {
     GET, POST /todos
     GET, PUT, DELETE /todos/:id
   </pre>
-  `.trim(),
+  `.trim()
   );
 });
 
 app.listen(Number(port), "0.0.0.0", () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
