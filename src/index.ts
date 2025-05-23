90747588cae5b48d57348eaae852536d7037b79a
avellin0 <daviavelino2007@gmail.com>
2025-05-23 16:02:06 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
+// import { PrismaClient } from "@prisma/client";
 import express from "express";
 import route from "./router/routes";
+
+// const prisma = new PrismaClient();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.raw({ type: "application/vnd.custom-type" }));
+app.use(express.text({ type: "text/html" }));
 app.use(express.static("assets"));
 app.use(route)
 
