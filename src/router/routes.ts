b82206a8db402bab4b6e33da4f3b88b269c4678c
avellin0 <daviavelino2007@gmail.com>
2025-05-23 16:08:58 -0300

    update

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,10 +2,9 @@ import { Router} from "express"
 import path from "path"
 const route = Router()
 
-
 route.get("/", (req,res) => {
 
-    res.sendFile(path.join(__dirname, "..","public", "index.html"))
+    res.sendFile(path.join(__dirname, "../../","public", "index.html"))
 })
 
 export default route;
