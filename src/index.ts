30c784bbd7af379bf5b993cc4a1516fbd0b2d104
avellin0 <daviavelino2007@gmail.com>
2025-05-23 15:59:24 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,14 @@
-import { PrismaClient } from "@prisma/client";
 import express from "express";
-
-const prisma = new PrismaClient();
+import route from "./router/routes";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.raw({ type: "application/vnd.custom-type" }));
-app.use(express.text({ type: "text/html" }));
 app.use(express.static("assets"));
+app.use(route)
 
-app.get("/artists", async (req, res) => {
-  const todos = await prisma.artist.findMany();
 
-  res.json(todos);
-});
-
-app.post("/artist", async (req, res) => {
-  const { name } = req.body;
-
-  const todo = await prisma.artist.create({
-    data: {
-      name,
-    },
-  });
-
-  return res.json(todo);
-});
-
-app.get("/todos/:id", async (req, res) => {
-  const id = req.params.id;
-  const todo = await prisma.artist.findUnique({
-    where: { id },
-  });
-
-  return res.json(todo);
-});
-
-app.get("/", async (req, res) => {
-  res.send(
-    `
-  <h1>Hello world!</h1>
-  <h2>Tranquilidade seus peida jontex?</h2>
-  <p>Sejam muito bem vindos ao meu canto</p>
-  <img src="/fotinha.jpg" alt="fotinha">
-  `.trim()
-  );
-});
-
-app.listen(Number(port), "0.0.0.0", () => {
+app.listen(3000, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
---
+++ src/router/routes.ts
@@ -0,0 +1,11 @@
+import { Router} from "express"
+import path from "path"
+const route = Router()
+
+
+route.get("/", (req,res) => {
+
+    res.sendFile(path.join(__dirname, "..","public", "index.html"))
+})
+
+export default route;
